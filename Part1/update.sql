a5371dbb4e2dfd1d41e0da7abaed4419bc4d9749
Daniel Wolpert <58397826+dan5000w@users.noreply.github.com>
2022-03-23 18:38:45 +0200

    Create update.sql

---
+++ Part1/update.sql
@@ -0,0 +1,15 @@
+update department
+set depname = 'bakery'
+where depid = 2
+
+update products
+set price = 12
+where productname = 'Shug'
+
+update persondetails
+set salary = 150000
+where workername = 'Lea'
+
+update products
+set price = 69
+where productname = 'Gahnoon'
